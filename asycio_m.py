cc995dd38fdfd46f7134e9e70f8e9bea6abe67a3
Braison Wabwire <braisonwabwire2003@gmail.com>
2025-06-16 12:46:01 +0300

    Trying different tasks

--- asycio_m.py
+++ asycio_m.py
@@ -1,18 +1,22 @@
 import asyncio
 
 # Define a coroutine that simulates a time consuming task
-async def fetch_data(delay):
-    print("Fetching data ...")
+async def fetch_data(delay, id):
+    print("Fetching data ...id:", id)
     await asyncio.sleep(delay)
-    print("data fetched")
-    return {"data":"some data"}
+    print("data fetched....id:",id)
+    return {"data":"some data","id":id}
 
 #Another coroutine that calls the first coroutine
 async def main():
-    print("start on main coroutine")
-    task=fetch_data(2)
-    result= await task
-    print(f'Recieved result:{result}')
-    print("End of main coroutine")
+    task1=fetch_data(2,1)
+    task2=fetch_data(2,2)
 
+    result1=await task1
+    print(f"recieved result: {result1}")
+    result2=await task2
+    print(f"Recieed results:{result2}")
+   
+   
+# Run the main coroutine
 asyncio.run(main())
