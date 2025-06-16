4ed4a3719f42031579ccaf337b0dfae0ac386143
Braison Wabwire <braisonwabwire2003@gmail.com>
2025-06-16 12:29:06 +0300

    demostrating await

--- asycio_m.py
+++ asycio_m.py
@@ -1,6 +1,18 @@
 import asyncio
 
+# Define a coroutine that simulates a time consuming task
+async def fetch_data(delay):
+    print("Fetching data ...")
+    await asyncio.sleep(delay)
+    print("data fetched")
+    return {"data":"some data"}
+
+#Another coroutine that calls the first coroutine
 async def main():
     print("start on main coroutine")
+    task=fetch_data(2)
+    result= await task
+    print(f'Recieved result:{result}')
+    print("End of main coroutine")
 
 asyncio.run(main())
